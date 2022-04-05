a52ab94bd4d92d4dead9bab999e9b939e1bce772
cairomurphy <84107491+cairomurphy@users.noreply.github.com>
2022-04-05 14:06:57 -0600

    started to get the admin to work

---
+++ AuthenticationLab/Pages/AddCrash.cshtml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthenticationLab.Infrastructure;
+using AuthenticationLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AuthenticationLab.Pages
+{
+    public class AddCrashModel : PageModel
+    {
+        private ICrashRepository repo { get; set; }
+        //public Basket basket { get; set; }
+        public string ReturnUrl { get; set; }
+
+        public AddCrashModel(ICrashRepository temp /*Basket b*/)
+        {
+            repo = temp;
+            //basket = b;
+        }
+
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+        }
+
+        public IActionResult OnPost(int crashId, string returnUrl)
+        {
+            Crash c = repo.mytable.FirstOrDefault(x => x.CRASH_ID == crashId);
+
+            //basket.AddItem(b, 1);
+
+            return RedirectToPage(new { ReturnUrl = returnUrl });
+        }
+
+        public IActionResult OnPostRemove(int bookId, string returnUrl)
+        {
+            //basket.RemoveItem(basket.Items.First(x => x.Book.BookId == bookId).Book);
+
+            return RedirectToPage(new { ReturnUrl = returnUrl });
+        }
+    }
+}
--- AuthenticationLab/Pages/Admin/Index.cshtml.cs
+++ AuthenticationLab/Pages/Admin/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AuthenticationLab.Views.Admin
+namespace AuthenticationLab.Pages.Admin
 {
     public class IndexModel : PageModel
     {
