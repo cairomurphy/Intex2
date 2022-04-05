7b21c4eacb6217bbd5cb3d9792d309a62ea67407
dannybellboy <90338634+dannybellboy@users.noreply.github.com>
2022-04-05 12:36:51 -0600

    details view

--- AuthenticationLab/Controllers/HomeController.cs
+++ AuthenticationLab/Controllers/HomeController.cs
@@ -69,6 +69,12 @@ namespace AuthenticationLab.Controllers
             return View();
         }
 
+        public IActionResult Details(long crashid)
+        {
+            var x = repo.mytable
+                .Single(x => x.CRASH_ID == crashid);
+            return View(x);
+        }
 
 
 
